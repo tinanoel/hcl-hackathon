e46623297c4588d43b7f710d2075f220b826c70e
tinanoel <42848076+tinanoel@users.noreply.github.com>
2025-05-17 13:49:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,10 @@ variable "public_subnet_cidrs" {
  description = "Public Subnet CIDR values"
  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
+
+variable "azs" {
+ type = list(string)
+ description = "availability zones"
+ default = ["us-east-1a","us-east-1b"]
+}
+
