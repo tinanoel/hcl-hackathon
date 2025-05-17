c5495a71ff81007d6ac24d1cce17406f754a07d4
tinanoel <42848076+tinanoel@users.noreply.github.com>
2025-05-17 13:47:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "public_subnet_cidrs" {
  type        = list(string)
  description = "Public Subnet CIDR values"
- default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+ default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
