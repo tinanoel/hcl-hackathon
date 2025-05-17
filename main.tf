b586f38cd1ba2a92ccee61d9b08e9a9e80d0b414
emmy06.nar <emmy06.nar@gmail.com>
2025-05-17 13:43:36 +0530

    added files

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+
+   cidr_block = "10.0.0.0/16"
+   tags = {
+
+     Name = "Project VPC"
+   }
+}
+
+resource "aws_subnet" "public_subnets" {
+ count      = length(var.public_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.public_subnet_cidrs, count.index)
+
+ tags = {
+   Name = "Public Subnet ${count.index + 1}"
+ }
+}
