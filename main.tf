1608852cf2eaac7d784c85e164510f1f8d1de932
tinanoel <42848076+tinanoel@users.noreply.github.com>
2025-05-17 13:51:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ resource "aws_subnet" "public_subnets" {
  count      = length(var.public_subnet_cidrs)
  vpc_id     = aws_vpc.main.id
  cidr_block = element(var.public_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
 
  tags = {
    Name = "Public Subnet ${count.index + 1}"
